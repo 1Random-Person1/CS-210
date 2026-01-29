489ff7f4eb4b281c7f622f39ee58adbb7070b046
Dat <nothisisatest10@gmail.com>
2026-01-28 17:44:00 -0800

    testing commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+using std::string;
+
+// R-1.1
+// D is not valid due to it having special characters
+
+// R - 1.3
+struct Pair {
+    int first;
+    double second;
+};
+
+// R - 1.4
+// abcabcdabc
+
+// R - 1.6
+
+double* dp[10]
+for (int i = 0; i < 10; i++) {
+    dp[i] = new double;
+    *dp[i] = 0.0;
+}
+
+
+
 int main() {
-    std::cout << "Mission Control is ready!" << std::endl;
     return 0;
 }
