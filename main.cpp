db1a48d93cc594fd048f23df9a67e649dbecd698
Dat <nothisisatest10@gmail.com>
2026-01-28 21:30:02 -0800

    c - 1.2 doen

--- main.cpp
+++ main.cpp
@@ -4,19 +4,23 @@
 using namespace std;
 using std::string;
 
-// R-1.1
+//Reinforcements ------------
+
+// R-1.1 ------------
 // D is not valid due to it having special characters
 
-// R - 1.3
+// R - 1.3 ------------
+
 struct Pair {
     int first;
     double second;
 };
 
-// R - 1.4
-// abcabcdabc
+// R - 1.4 ------------
 
-// R - 1.6
+abcabcdabc
+
+// R - 1.6 ------------
 
 double* dp[10]
 for (int i = 0; i < 10; i++) {
@@ -24,7 +28,7 @@ for (int i = 0; i < 10; i++) {
     *dp[i] = 0.0;
 }
 
-// R - 1.11
+// R - 1.11 ------------
 
 class Flower {
 
@@ -65,7 +69,56 @@ public:
     }
 };
 
-
 int main() {
+    Flower flower = new Flower("Red Rose", 5, 2.99);
+    std::cout << flower.getName() << std::endl;
+    std::cout << flower.getPetals() << std::endl;
+    std::cout << flower.getPrice() << std::endl;
+
     return 0;
 }
+
+//Creativity ------------
+
+// C - 1.2 ------------
+
+bool evenProduct(int a[], int n) {
+
+    if (n < 2) {
+        return false;
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 == 0) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    int list[] = {2, 5, 6};
+    bool result = evenProduct(list, 3);
+    if (result) {
+        std::cout << "Product is even \n";
+    } else {
+        std::cout << "Product is odd \n";
+    }
+
+    return 0;
+}
+
+// C - 1. ------------
+
+
+
+
+
+// C - 1. ------------
+
+
+
+
+
+
